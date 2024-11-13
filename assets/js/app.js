21ec97016834300764f1c2262b47dd3e0b75bf69
Tiago Gonçalves <tiago.goncalves.faria@gmail.com>
2024-11-13 09:31:31 +0100

    Created new portfolio

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-/*const imageName = document.querySelectorAll('.card_container .project_card span ')
-
-Array.from(imageName).forEach(element => {
-    let imageEl = element.parentNode.parentNode
-    let imagePath = element.innerText.toLowerCase() + '.jpg'
-
-    imageEl.style.background = 'url(./assets/images/' + imagePath +')'
-    imageEl.style.backgroundSize = 'cover'
-})*/
